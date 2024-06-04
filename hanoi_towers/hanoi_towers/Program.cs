6158c535ccd6a18fe5a90b3ce12d1d3cc6f5e8ce
Furkan Dulkadir <115067547+furkandlkdr@users.noreply.github.com>
2024-06-04 15:08:18 +0300

    Added comments

--- hanoi_towers/hanoi_towers/Program.cs
+++ hanoi_towers/hanoi_towers/Program.cs
@@ -1,22 +1,26 @@
 ﻿namespace hanoi_towers{
     internal class Program{
         class Stick{
+            // Define the stick class, which has a name and an array of disks
             public string name { get; set; }
             public int[] diskArray { get; set; }
             public Stick(string name, int diskCount){
                 this.name = name;
                 diskArray = new int[diskCount];
             }
+            // Load the disk to the stick
             public void loadDisk(int diskSize){
                 for (int i = 1; i < diskArray.Length + 1; i++)
                     this.diskArray[i - 1] = diskArray.Length + 1 - i;
             }
+            // Print the disk in the stick
             public void printInside(){
                 foreach (int disk in diskArray)
                     Console.Write(disk + " ");
                 Console.WriteLine();
             }
         }
+        // Move the disk from source to destination
         static void hanoi(int n, Stick source, Stick destination, Stick middle){
             if (n == 1)
                 Console.WriteLine("Moved the top disk from " + source.name + " to " + destination.name);
@@ -29,17 +33,23 @@
         static void Main(string[] args){
             Console.WriteLine("How many disk we have in game?");
             int diskCount = Convert.ToInt32(Console.ReadLine());
+            // Create 3 sticks
             Stick sourceStick = new Stick("A", diskCount);
             Stick destStick = new Stick("B", diskCount);
             Stick midStick = new Stick("C", diskCount);
             sourceStick.loadDisk(diskCount);
+            // Print the disk in the source stick
             Console.WriteLine("Current sourceStick:");
             sourceStick.printInside();
+            // Start the game
             hanoi(diskCount, sourceStick, destStick, midStick);
+            // Print the disk in the sticks after the game
             Console.WriteLine("Source stick:");
             sourceStick.printInside();
+            // Print the disk in the destination stick
             Console.WriteLine("\nDestination stick:");
             destStick.printInside();
+            // Print the disk in the middle stick
             Console.WriteLine("\nMiddle stick:");
             midStick.printInside();
         }
